bab2a4c085f1b1a4d34cfb1964c87c9e96b3a923
Adrian Guerrero <adrian.guerrero.martinez.1990@gmail.com>
2025-02-03 20:07:25 +0100

    UPDATE Clase7: Introducción de JavaDoc a la clase7. Añadido título a JavaDoc

--- src/Clases/Clase7.java
+++ src/Clases/Clase7.java
@@ -5,11 +5,11 @@ import org.w3c.dom.ls.LSOutput;
 public class Clase7 {
 
     /**
-     * La clase 7 proporciona la resolución matemática de logaritmos y exponenciales
+     * LOGARITMOS Y EXPONENCIALES
+     * Esta clase proporciona la resolución matemática de logaritmos y exponenciales
      * @author Adrián Guerrero Martínez
      * @version 1.0
      */
-    //LOGARITMOS Y EXPONENCIALES
 
     // 1: LOGARITMO NATURAL
     public double log(double x) {
