b1ebf39fab92805c3414542b215a6ce38a9f7164
Adrian Guerrero <adrian.guerrero.martinez.1990@gmail.com>
2025-02-03 19:49:01 +0100

    UPDATE Clase7: Desarrollo del método 1 (Logaritmo natural)

--- src/Clases/Clase7.java
+++ src/Clases/Clase7.java
@@ -1,4 +1,18 @@
 package Clases;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Clase7 {
+
+    //LOGARITMOS Y EXPONENCIALES
+
+    // 1: LOGARITMO NATURAL
+    public double log(double x) {
+        return (Math.log(x));
+    }
+
+    // 2: LOGARITMO DECIMAL
+
+    // 3: EXPONENCIAL
+
 }
