0cf3362030406d1c941f5e7f060e58ac561c969b
Adrian Guerrero <adrian.guerrero.martinez.1990@gmail.com>
2025-02-09 20:09:45 +0100

    UPDATE Clase LogaritmosExponencialesTest: Creación de la clase de pruebas unitarias para la clase LogaritmosExponenciales

---
+++ src/Test/LogaritmosExponencialesTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogaritmosExponencialesTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void log() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void log10() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void exp() {
+    }
+}
