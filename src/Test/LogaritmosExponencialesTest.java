a7ea9c8f6bba20ea279f0b44a636e58690e08bf8
Adrian Guerrero <adrian.guerrero.martinez.1990@gmail.com>
2025-02-09 20:18:36 +0100

    UPDATE Clase LogaritmosExponencialesTest: Creación del BeforeEach

--- src/Test/LogaritmosExponencialesTest.java
+++ src/Test/LogaritmosExponencialesTest.java
@@ -1,21 +1,29 @@
 package Test;
 
+import Clases.LogaritmosExponenciales;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LogaritmosExponencialesTest {
 
+    LogaritmosExponenciales prueba;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        prueba = new LogaritmosExponenciales();
     }
 
+    //PRUEBAS UNITARIAS PARA LOGARITMOS NATURALES
     @org.junit.jupiter.api.Test
     void log() {
     }
 
+    //PRUEBAS UNITARIAS PARA LOGARITMOS DECIMALES
     @org.junit.jupiter.api.Test
     void log10() {
     }
 
+    //PRUEBAS UNITARIAS PARA FUNCIONES EXPONENCIALES
     @org.junit.jupiter.api.Test
     void exp() {
     }
