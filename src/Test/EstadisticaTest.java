737f080c18ca4a1a9f13c3ce31cdcd10df9fea89
MarianoCriado <mariano.criado636@comunidadunir.net>
2025-02-16 17:16:46 +0100

    UPDATE: Refactor

---
+++ src/Test/EstadisticaTest.java
@@ -0,0 +1,9 @@
+package Test;
+import Clases.Estadistica;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EstadisticaTest {
+    @org.junit.jupiter.api.Test
+
+}
