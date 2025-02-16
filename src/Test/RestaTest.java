b9da1fd203c55e0ea1838869fe098fc6df807f09
lucellymon <lucelly.montes387@comunidadunir.net>
2025-02-16 17:21:38 +0100

    añado cambios en la clase test resta

--- src/Test/RestaTest.java
+++ src/Test/RestaTest.java
@@ -3,9 +3,9 @@ package Test;
 import Clases.Resta;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 
 class RestaTest {
@@ -15,56 +15,51 @@ class RestaTest {
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         // Se ejecuta antés de cada test.
-        resta = new Resta();
+        Resta = new Resta();
     }
 
 
-    @org.junit.jupiter.api.Test
-    void getValorAcumulado() {
-        // se inicia en 0.
-        double inicio = resta.getValorAcumulado();
-        assertEquals(0.0, inicio, "error");
-        //Hacemos una resta acumulada.
-        resta.getValorAcumulado();
-        double resultado = resta.restarAcumulado(10.5);
-        assertEquals(-10.5, resultado, "error");
 
+        @org.junit.jupiter.api.Test
+        // prueba que el valor inicial sea 0.
+        void getValorAcumulado () {
+        int valorInicial=0;
+        assert
+
+
+
+
+        }
+
+
+        @org.junit.jupiter.api.Test
+        void restar () {
+            // Comprueba la resta de dos números enteros.
+
+            int resultado = resta.restar(20, 5);
+            assertEquals(15, resultado);
+
+
+        }
+
+        @org.junit.jupiter.api.Test
+        void restarReales () {
+            double resultado = resta.restarReales(3.5, 2.2);
+            assertEquals(1.3, resultado);
+
+        }
+        @org.junit.jupiter.api.Test
+        void restarRealesTres () {
+            double resultado = resta.restarRealesTres(2.7, 6.9, 1.3);
+            assertEquals(-5.5, resultado);
+        }
+
+        @org.junit.jupiter.api.Test
+        void restarAcumulado () {
+
+        }
     }
 
-    @org.junit.jupiter.api.Test
-        // Realiza la resta de dos números enteros.
-    void restar() {
-        int resultado = resta.restar(10, 5);
-        assertEquals(5, resultado, "error");
-
-    }
-
-    @org.junit.jupiter.api.Test
-        //Realiza la resta de dos números reales.
-    void restarReales() {
-        double resultado = resta.restarReales(3.5, 2.2);
-        assertEquals(1.3, resultado, "error");
-
-    }
-
-    @org.junit.jupiter.api.Test
-        // Realiza la resta de tres números reales
-    void restarRealesTres() {
-        double resultado = resta.restarRealesTres(2.7, 6.9, 1.3);
-        assertEquals(-5.5, resultado, "error");
-    }
-
-    @org.junit.jupiter.api.Test
-        //Prueba
-    void restarAcumulado() {
-        resta.restarAcumulado(10.0);
-        double resultado = resta.restarAcumulado(10.0);
-        assertEquals(-10.0, resultado, "error");
-
-
-    }
-
-}
 
 
 
