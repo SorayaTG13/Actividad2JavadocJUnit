7b9ba1186b12608b3827e68138b0b51a26c9c45c
smarcos90 <smarcosg90@gmail.com>
2025-02-17 17:02:29 +0100

    CREATE: Creación TestCociente

--- src/Test/CocienteTest.java
+++ src/Test/CocienteTest.java
@@ -1,62 +1,67 @@
 package Test;
+
 import Clases.Cociente;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class CocienteTest {
+    private final Cociente cociente = new Cociente();
 
-    // Prueba division números reales.
-    @org.junit.jupiter.api.Test
-    void cocienteReales (){
-
-        double resultado = Cociente.cocienteReales(-77.80, 77.80);
-        assertEquals (1, resultado);
+    //División entre dos números reales
+    @Test
+    public void testDividirReales() {
+        double resultado = Cociente.cocienteReales(5.0,2.5);
+        assertEquals(2.0, resultado, "El resultado debería ser 2.0");
     }
 
-    // Prueba division números reales entre 0.
-    @org.junit.jupiter.api.Test
-    void cocienteRealesCero (){
-
-        assertThrows(ArithmeticException.class, () -> Cociente.cocienteReales(54.43, 0));
+    //División dos números reales entre 0
+    @Test
+    public void testDividirReales0() {
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> {
+            Cociente.cocienteReales(5.0, 0.0);
+        });
+        assertEquals("Error. No es posible dividir entre 0", exception.getMessage());
     }
 
-    // Prueba division enteros.
-    @org.junit.jupiter.api.Test
-    void cocienteEnteros (){
-
-        double resultado = Cociente.cocienteEnteros(44, 2);
-        assertEquals (22, resultado);
+    //División dos números enteros
+    @Test
+    public void testDividirEnteros(){
+        int resultado = Cociente.cocienteEnteros(4,2);
+        assertEquals(2, resultado, "El resultado debería ser 2");
     }
 
-    // Prueba division números enteros entre 0.
-    @org.junit.jupiter.api.Test
-    void cocienteRealesCero (){
-
-        assertThrows(ArithmeticException.class, () -> Cociente.cocienteReales(54, 0));
+    //División dos números enteros entre 0
+    @Test
+    public void testDividirEnteros0(){
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> {
+            Cociente.cocienteEnteros(5,0);
+        });
+        assertEquals("Error. No es posible dividir entre 0", exception.getMessage());
     }
 
-    // Prueba division inversa.
-    @org.junit.jupiter.api.Test
-    void cocienteInverso (){
-
-        double resultado = Cociente.cocienteInverso(4);
-        assertEquals (0.25, resultado);
+    //Inverso
+    @Test
+    public void testInverso(){
+        double resultado = Cociente.cocienteInverso(5);
+        assertEquals(0.2, resultado, "El resultado debería ser 0.2");
     }
 
-    // Prueba raiz cuadrada.
-    @org.junit.jupiter.api.Test
-    void raizCuadrada (){
-
+    //Raíz cuadrada
+    @Test
+    public void testRaiz(){
         double resultado = Cociente.raizCuadrada(4);
-        assertEquals (2, resultado);
+        assertEquals(2.0, resultado, "El resultado debería ser 2.0");
     }
 
-    // Prueba raiz cuadrada negativa
-    @org.junit.jupiter.api.Test
-    void raizNegativa (){
-
-        assertThrows(ArithmeticException.class, () -> Cociente.raizCuadrada(-1));
-
+    //Raíz cuadrada número negativo
+    @Test
+    public void testRaizNegativo() {
+        ArithmeticException exception = assertThrows(ArithmeticException.class, () -> {
+            Cociente.raizCuadrada(-8);
+        });
+        assertEquals("Error. No es posible calcular la raíz cuadrada de un número negativo", exception.getMessage());
     }
+
+
 }
