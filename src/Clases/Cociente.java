a7afc12633b27699b9711caad1ad326b04b2492f
smarcos90 <smarcosg90@gmail.com>
2025-02-20 15:09:29 +0100

    UPDATE: actualización ClaseCociente

--- src/Clases/Cociente.java
+++ src/Clases/Cociente.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
  *Clase que implementará todos los métodos que están relacionados con la división de la
  * Calculadora.
  *
- * @author: Sandra Marcos García (AKA @smarcos90 via: GitHub)
- * @see <a href=https://github.com/SorayaTG13/Actividad2JavadocJUnit.git>
+ * @author: Sandra Marcos Garcia (AKA smarcos90 via: GitHub)
+ * @see <a href=https://github.com/SorayaTG13/Actividad2JavadocJUnit.git></a>
  * @version: 1.0
  */
 
